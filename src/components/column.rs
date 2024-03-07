71a799627c4079dc90b772a98a2c014c6dfa7f17
Romulo-Moraes <romulo.moraes.dev@gmail.com>
2024-03-07 12:34:33 -0300

    Create column component

---
+++ src/components/column.rs
@@ -0,0 +1,90 @@
+use std::io::{Stdout, Write};
+
+use rand::{self, Rng};
+use crossterm::{self, cursor::MoveTo, QueueableCommand};
+
+#[derive(Debug)]
+pub struct Column {
+  characters : String,
+  length : u16,
+  column : u16,
+  row : u16
+}
+
+impl Column {
+  pub fn new(terminal_columns : u16) -> Self {
+    return Column {
+      characters : column_characters(terminal_columns),
+      length : column_length(),
+      column : column_position(terminal_columns),
+      row : 1
+    }
+  }
+
+  pub fn increment_row(&mut self) {
+    self.row += 1;
+  }
+
+  pub fn draw(&self, terminal_rows : u16, stdout : &mut Stdout) {
+
+    let visible_position = match self.row.checked_sub(self.length) {
+      Some(value) => if value == 0 {1} else {value},
+      None => 1
+    };
+
+    stdout.queue(MoveTo(self.column, self.row))
+     .expect("Error on set cursor position");
+
+    for i in (visible_position..self.row).rev() {
+      if i > terminal_rows {
+        continue;
+      }
+
+      stdout.write((&self.characters[(i as usize)-1..(i as usize)])
+       .as_bytes())
+        .expect("Error on write column on terminal");
+
+      stdout.queue(MoveTo(self.column, i))
+       .expect("Error on update cursor position");
+    }
+  }
+
+  pub fn out_of_visible_area(&self, terminal_rows : u16) -> bool {
+    match self.row.checked_sub(self.length) {
+      Some(column_tail) => {
+        if column_tail > terminal_rows {
+          return true;
+        }
+
+        return false;
+      },
+      None => false
+    }
+  }
+}
+
+fn column_position(columns : u16) -> u16 {
+  let mut rng = rand::thread_rng();
+
+  return rng.gen_range(0..columns);
+}
+
+fn column_length() -> u16 {
+  let mut rng = rand::thread_rng();
+
+  return rng.gen_range(5..=18);
+}
+
+fn column_characters(terminal_columns : u16) -> String {
+  let available_characters = "ab1cd2ef3gh4ij5kl6mn7op8qr9st0uvwxyz";
+  let mut characters = String::new();
+  let mut rng = rand::thread_rng();
+  let mut slice_index;
+
+  for _ in 0..terminal_columns {
+    slice_index = rng.gen_range(1..=available_characters.len());
+    characters.push_str(&available_characters[slice_index-1..slice_index]);
+  }
+
+  return characters;
+}
