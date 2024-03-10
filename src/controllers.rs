b59e5d93319681ead17edc54fa0ed7289cf1ba1f
Romulo-Moraes <romulo.moraes.dev@gmail.com>
2024-03-10 11:47:12 -0300

    Implement terminal resize handling

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,14 +1,16 @@
-use std::{io::stdin, sync::{mpsc::Sender, Arc, Mutex}, thread, time::Duration};
+use std::{sync::{atomic::{AtomicU16, Ordering}, mpsc::Sender, Arc, Mutex}, thread, time::Duration};
+
+use crossterm::event::{read, Event, KeyCode};
 
 use crate::components::column::Column;
 
-pub fn column_creator_controller(creation_timeout : u64, mutex : Arc<Mutex<Vec<Column>>>, terminal_columns : u16) {
+pub fn column_creator_controller(creation_timeout : u64, mutex : Arc<Mutex<Vec<Column>>>, terminal_columns : Arc<AtomicU16>) {
   thread::spawn(move || {
     loop {
       {
         let mut columns_vector = mutex.lock().unwrap();
 
-        columns_vector.push(Column::new(terminal_columns));
+        columns_vector.push(Column::new(terminal_columns.load(Ordering::SeqCst)));
       }
 
       thread::sleep(Duration::from_millis(creation_timeout));
@@ -18,10 +20,15 @@ pub fn column_creator_controller(creation_timeout : u64, mutex : Arc<Mutex<Vec<C
 
 pub fn exit_program_controller(tx_channel : Sender<bool>) {
   thread::spawn(move || {
-    let mut input = String::new();
-
-    stdin().read_line(&mut input).expect("Could not read user input");
-
-    tx_channel.send(true).expect("Could not send exit program message through the channel ");
+    loop {
+      match read().unwrap(){
+        Event::Key(event) => {
+          if event.code == KeyCode::Enter {
+            tx_channel.send(true).expect("Could not send message to stop the program")
+          }
+        },
+        _ => {}
+      }
+    }
   });
 }
--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,34 @@ mod components;
 mod arguments;
 mod colors;
 
-use std::{io::{stdout, Write}, sync::{mpsc, Arc, Mutex}, thread, time::Duration};
+use std::{io::{stdout, Write}, sync::{atomic::{AtomicU16, Ordering}, mpsc, Arc, Mutex}, thread, time::Duration};
 use arguments::{vec_to_array, Args};
 use clap::Parser;
 use components::column::Column;
-use crossterm::{cursor::{self}, terminal::{size, Clear, ClearType}, ExecutableCommand, QueueableCommand};
+use crossterm::{cursor, execute, terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen}, QueueableCommand};
 
 fn main() {
   let (exit_program_tx, exit_program_rx) = mpsc::channel();
-  let (terminal_width, terminal_height) = size().unwrap();
+  let (terminal_width, mut terminal_height) = size().unwrap();
   let mut stdout = stdout();
   let mutex: Arc<Mutex<Vec<Column>>> = Arc::new(Mutex::new(Vec::new()));
   let program_arguments = Args::parse();
+  let terminal_width = Arc::new(AtomicU16::new(terminal_width));
   let program_colors = (
     vec_to_array!(program_arguments.edge_color),
     vec_to_array!(program_arguments.body_color)
   );
-  
-  controllers::column_creator_controller(program_arguments.column_interval, Arc::clone(&mutex), terminal_width);
+
+  controllers::column_creator_controller(program_arguments.column_interval, Arc::clone(&mutex), Arc::clone(&terminal_width));
   controllers::exit_program_controller(exit_program_tx);
 
-  stdout.execute(cursor::Hide).expect("Could not hide the cursor");
-  stdout.queue(Clear(ClearType::All))
-     .expect("Could not clear the terminal");
+  execute!(
+    stdout,
+    EnterAlternateScreen,
+    cursor::Hide
+  ).expect("Could not configure terminal to run the application");
+
+  enable_raw_mode().expect("Could not enable raw mode");
 
   while
     match exit_program_rx.try_recv() {
@@ -35,6 +40,17 @@ fn main() {
   {
     {
       let mut vector_mutex = mutex.lock().unwrap();
+
+      let (new_width, new_height) = size().expect("err");
+
+      if terminal_width.load(Ordering::SeqCst) != new_width || terminal_height != new_height {
+        terminal_width.store(new_width, Ordering::SeqCst);
+        terminal_height = new_height;
+
+        vector_mutex.clear();
+
+        stdout.queue(Clear(ClearType::All)).expect("Could not clear terminal screen");
+      }
 
       vector_mutex.retain_mut(|e| {        
         e.increment_row();
@@ -60,5 +76,12 @@ fn main() {
     thread::sleep(Duration::from_millis(program_arguments.render_speed));
   }
 
-  stdout.execute(cursor::Show).expect("Could not show the cursor");
+  execute!(
+    stdout,
+    LeaveAlternateScreen,
+    cursor::Show
+  ).expect("Could not reset the terminal");
+
+  disable_raw_mode().expect("Could not disable raw mode");
+  
 }
